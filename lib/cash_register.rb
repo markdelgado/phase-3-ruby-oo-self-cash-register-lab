8a1392217728c3418ad7b1ba33d6513af3cf337f
Mark Delgado <markdelgado.md@hotmail.com>
2022-08-12 15:23:35 -0400

    commit tree 13fb576f663358f9963cb17586a4a257398f710b

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+   attr_accessor :total, :discount, :items, :previous_total
+   
+    def initialize(discount=0)
+        @total= 0
+        @discount= discount
+        @items=[]
+        
+    end
 
+    def add_item(title, price)
+        
+    
+
+    
+    end
+
+end
